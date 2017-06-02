1f971c980d3ccac82e5ecc080509c514e0ad231a
robertpapoyan <robertpapoyan96@gmail.com>
2017-06-02 16:46:44 +0400

    Add files via upload

--- Prob1x9x.java
+++ Prob1x9x.java
@@ -7,20 +7,8 @@ public class Prob1x9x {
         String word = s.nextLine();
 
         char[] cWord = word.toCharArray();
-        char [] myWord = new char[cWord.length];
-        for (int i = 0; i < cWord.length; i++ ){
-            myWord[i] = word.charAt(i);
-        }
-
-        char [] changedWord = new char[cWord.length];
-        int j = 0;
         for (int i = cWord.length; i >= 0; i--){
-            changedWord[j] = cWord[j];
-            j = j +1;
-            if (j > cWord.length){
-                break;
-            }
+            System.out.println(cWord[i]);
         }
-
     }
 }
