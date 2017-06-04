6a8bba237280b8e30f20e29562f0f675195485b0
Harutyun <haruthovhanns@gmail.com>
2017-06-05 01:18:41 +0400

    Update Prob1x9x.java
    erased code size, solved problem without unnecessary variable (char array).

--- Prob1x9x.java
+++ Prob1x9x.java
@@ -5,11 +5,9 @@ public class Prob1x9x {
 
         Scanner s = new Scanner(System.in);
         String word = s.nextLine();
-
-
-        char[] cWord = word.toCharArray();
-        for (int i = cWord.length - 1; i >= 0; i--){
-            System.out.print(cWord[i]);
+        for (int i = word.length() - 1; i>=0 ;i-- ) 
+        {
+            System.out.print(word.charAt(i));
         }
     }
 }
